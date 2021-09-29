7fc237c0b6e2b832d8ea094902bb042242c2fe12
Nathan B <nathanbritodasilva@gmail.com>
2021-09-29 14:30:40 -0300

    PDG - Added pokemonspecies class
    Class returns proper variety, proper flavor text and genus. Use id in proper variety url for alola variant if it exists.

--- Software/PokemonDataObtainer/main.py
+++ Software/PokemonDataObtainer/main.py
@@ -1,23 +1,16 @@
-# This is a sample Python script.
+from time import sleep
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+from models.pokedex import Pokedex
+from models.pokemonspecies import PokemonSpecies
 
-import pokebase as pb
-
-from pokedex import Pokedex
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
     test = Pokedex(17)
     for i in test.pokemon_entries:
-        print(i.pokemon_species.name)
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+        pokemonspecies = PokemonSpecies(i.pokemon_species.id)
+        print("--------------------------------------------")
+        print("Name.: " + pokemonspecies.name)
+        print("Genera.: " + pokemonspecies.genera)
+        print("Flavor Text.: " + pokemonspecies.flavortext)
+        print("Variety got.: " + pokemonspecies.varieties)
+        print("--------------------------------------------")
+        sleep(1)

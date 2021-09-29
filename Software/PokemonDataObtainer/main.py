62884b7c6b67cfca6eeb510ea50b0df295ab5d6a
Nathan B <nathanbritodasilva@gmail.com>
2021-09-29 14:04:32 -0300

    PokemonDataObtained - Criado classe pokedex
    Classe para retornar a pokedex de uma região. Como o projeto se baseia em alola, via testes as entries necessárias serão de 17 a 20

--- Software/PokemonDataObtainer/main.py
+++ Software/PokemonDataObtainer/main.py
@@ -3,6 +3,10 @@
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
+import pokebase as pb
+
+from pokedex import Pokedex
+
 
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
@@ -12,5 +16,8 @@ def print_hi(name):
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('PyCharm')
+    test = Pokedex(17)
+    for i in test.pokemon_entries:
+        print(i.pokemon_species.name)
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/

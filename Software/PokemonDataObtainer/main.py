63d218f5a23d96bc4b933ee5079d43e68d3fb186
Nathan B <nathanbritodasilva@gmail.com>
2021-10-27 08:44:50 -0300

    PDG - Added range
    Included range of 17 to 21 to get all pokemon data from alola

--- Software/PokemonDataObtainer/main.py
+++ Software/PokemonDataObtainer/main.py
@@ -7,25 +7,26 @@ from models.pokemonspecies import PokemonSpecies
 import io
 
 if __name__ == '__main__':
-    test = Pokedex(17)
-    for i in test.pokemon_entries:
-        pokemonspecies = PokemonSpecies(i.pokemon_species.id)
-        pokemon = Pokemon(pokemonspecies.varieties)
-        print("--------------------------------------------")
-        f = open(str(i.pokemon_species.id) + ".txt", 'w')
-        print("Name.: " + pokemonspecies.name)
-        print("Genera.: " + pokemonspecies.genera)
-        print("Flavor Text.: " + pokemonspecies.flavortext)
-        #print("Sprite link.: " + pokemon.sprite)
-        urllib.request.urlretrieve(pokemon.sprite, str(i.pokemon_species.id) + ".png" )
+    for j in range(17,21):
+        test = Pokedex(j)
+        for i in test.pokemon_entries:
+            pokemonspecies = PokemonSpecies(i.pokemon_species.id)
+            pokemon = Pokemon(pokemonspecies.varieties)
+            print("--------------------------------------------")
+            f = open(str(i.pokemon_species.id) + ".txt", 'w')
+            print("Name.: " + pokemonspecies.name)
+            print("Genera.: " + pokemonspecies.genera)
+            print("Flavor Text.: " + pokemonspecies.flavortext)
+            # print("Sprite link.: " + pokemon.sprite)
+            urllib.request.urlretrieve(pokemon.sprite, str(i.pokemon_species.id) + ".png")
 
-        f.write("Name: " + pokemonspecies.name + '\n')
-        f.write("Genera: " + pokemonspecies.genera + '\n')
-        f.write("Flavor: " + pokemonspecies.flavortext + '\n')
+            f.write("Name: " + pokemonspecies.name + '\n')
+            f.write("Genera: " + pokemonspecies.genera + '\n')
+            f.write("Flavor: " + pokemonspecies.flavortext + '\n')
 
-        for i in (pokemon.stats.items()):
-            print("Stats " + i[0] + ".: " + str(i[1]))
-            f.write(i[0] + ": " + str(i[1]) + '\n')
-        f.close()
-        print("--------------------------------------------")
-        sleep(1)
+            for i in (pokemon.stats.items()):
+                print("Stats " + i[0] + ".: " + str(i[1]))
+                f.write(i[0] + ": " + str(i[1]) + '\n')
+            f.close()
+            print("--------------------------------------------")
+            sleep(1)
